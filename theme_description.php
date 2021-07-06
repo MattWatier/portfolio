657b6ebfc4fc0daab7d46894015cef1e358a9a3a
Matt Watier <mattwatier@gmail.com>
2021-07-06 09:52:04 -0400

    text edits

--- theme_description.php
+++ theme_description.php
@@ -1,8 +1,8 @@
 <?php
 // Zenphoto theme definition file
-$theme_description['name'] = 'Infographic Portfolio 2019';
+$theme_description['name'] = 'Infographics Portfolio 2019';
 $theme_description['author'] = gettext('<a href="http://mattwatier.com" target="_blank">Matt Watier</a>');
 $theme_description['version'] = '10.0.1';
 $theme_description['date'] = '3/5/2019';
-$theme_description['desc'] = gettext("Optimised for Load time and made it compatable with zenphoto 1.5.4");
+$theme_description['desc'] = gettext("Optimized for Load time and made it compatible with ZenP`hoto 1.5.4");
 ?>
